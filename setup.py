d259659c5287d284f86d30492653832b9103ac50
Manuel Sanchez del Rio <srio@esrf.eu>
2023-10-26 15:07:04 +0200

    new release

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ from setuptools import setup
 
 
 setup(name='crystalpy',
-      version='0.0.19',
+      version='0.0.20',
       description='Python crystal polarization calculation',
       author='Manuel Sanchez del Rio, Edoardo Cappelli, Mark Glass',
       author_email='srio@esrf.eu',
